cda515bafc6b0824d179b4d385ff4cb3fc62faa8
KZ <kjreem.imagine@gmail.com>
2022-07-23 21:16:00 +0430

    [feat]landing-page: created components

---
+++ src/app/landing-page/components/publishments/publishments.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PublishmentsComponent } from './publishments.component';
+
+describe('PublishmentsComponent', () => {
+  let component: PublishmentsComponent;
+  let fixture: ComponentFixture<PublishmentsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PublishmentsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PublishmentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -3,11 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page.routing';
 import { LadingPageRoutigComponent } from './components/lading-page-routig/lading-page-routig.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { StaffComponent } from './components/staff/staff.component';
+import { ProductsComponent } from './components/products/products.component';
+import { PublishmentsComponent } from './components/publishments/publishments.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    LadingPageRoutigComponent
+    LadingPageRoutigComponent,
+    NavBarComponent,
+    ServicesComponent,
+    AboutComponent,
+    StaffComponent,
+    ProductsComponent,
+    PublishmentsComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
