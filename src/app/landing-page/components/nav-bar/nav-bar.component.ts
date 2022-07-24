6099130837956fa765eed7968c4bee2aa69400e3
KZ <kjreem.imagnie@gmail.com>
2022-07-24 13:16:24 +0430

    [feat]landing-page: added tempalte for nav bar

--- src/app/landing-page/components/nav-bar/nav-bar.component.ts
+++ src/app/landing-page/components/nav-bar/nav-bar.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  navButtons = [
+    {
+      name: 'جستوجو',
+      rout: '',
+    },
+    {
+      name: 'خدمات',
+      rout: '',
+    },
+    {
+      name: 'ساعت ویزیت',
+      rout: '',
+    },
+    {
+      name: 'مقالات',
+      rout: '',
+    },
+    {
+      name: 'درباره ما',
+      rout: '',
+    },
+    {
+      name: 'ارطبات با ما',
+      rout: '',
+    },  
+    {
+      name: 'پرسش های متداول',
+      rout: '',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onClick(id: number) {}
 }
