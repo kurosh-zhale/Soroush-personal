92e00fa1d59d47e9c0844f889db7f4f1bb5f9d35
KZ <kjreem.imagine@gmail.com>
2022-07-25 21:35:26 +0430

    changed sizing and finished footer

--- src/app/landing-page/components/publishments/publishments.component.ts
+++ src/app/landing-page/components/publishments/publishments.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-publishments',
   templateUrl: './publishments.component.html',
-  styleUrls: ['./publishments.component.scss']
+  styleUrls: ['./publishments.component.scss'],
 })
 export class PublishmentsComponent implements OnInit {
+  publishments = [
+    {
+      image: '../../../../assets/images/komarov-egor-LlvSEbgjQoA-unsplash.jpg',
+      rout: '#',
+      name: 'Test',
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore porro sapiente at molestias harum voluptas officiis accusantium veritatis vel. Excepturi voluptatem saepe voluptatum similique suscipit non inventore obcaecati aliquam nulla.',
+    },
+    {
+      image: '../../../../assets/images/komarov-egor-LlvSEbgjQoA-unsplash.jpg',
+      rout: '#',
+      name: 'Test',
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore porro sapiente at molestias harum voluptas officiis accusantium veritatis vel. Excepturi voluptatem saepe voluptatum similique suscipit non inventore obcaecati aliquam nulla.',
+    },
+    {
+      image: '../../../../assets/images/komarov-egor-LlvSEbgjQoA-unsplash.jpg',
+      rout: '#',
+      name: 'Test',
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore porro sapiente at molestias harum voluptas officiis accusantium veritatis vel. Excepturi voluptatem saepe voluptatum similique suscipit non inventore obcaecati aliquam nulla.',
+    },
+  ];
 
-  constructor() { }
+  description!: string;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.filterDescription();
   }
 
+  filterDescription() {
+    this.publishments.map((el) => (this.description = el.desc.slice(0, 100)));
+    console.log(2);
+  }
 }
